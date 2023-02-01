6cf68048ee25374b2c7f064b3710094b062b88fd
Maximiliano Cattáneo <maximilianodamiancattaneo@gmail.com>
2023-02-01 14:49:02 +0100

    lab setup

---
+++ lab-vue-setup/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex-col">
+    <NewComponent/>
+    <div>Message: {{ mensaje }}</div>
+    <button @click="helloWord">Saludo</button>
+  </div>
+
+  
+</template>
+
+<script setup>
+import NewComponent from './components/NewComponent.vue'
+const mensaje = 'Soy un mensaje'
+function helloWord() {
+  alert(`Hello Word!`)
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ lab-vue-setup/src/components/NewComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-40 h-40 bg-red-200">
+        Soy un componente
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
